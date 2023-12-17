babe4355354b3c468578a4d706539809cee6eab7
NotF-21 <marcellreynaldoo@gmail.com>
2023-12-17 14:02:53 +0700

    revisi 4 17/12

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require('dotenv').config();
 
 const app = express();
 app.set("port", 3000);
--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 const API_KEY = "9bf25b372702bb91871f585a35997be0";
-const OPENAI_API_KEY = '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'; // Replace with your actual API key
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ""; // Replace with your actual API key
 
 const getCurrentWeather = async () => {
     const link = `https://api.openweathermap.org/data/2.5/weather?lat=-7.2459717&lon=112.7378266&appid=${API_KEY}&units=metric`;
@@ -103,7 +103,7 @@ router.get("/naration", async (req, res) => {
             image : image.image,
         });
     } catch (error) {
-        return res.status(500).send('Internal Server Error' + error.toString());
+        return res.status(500).send('Internal Server Error' + error.toString() + process.env);
     }
 });
 
