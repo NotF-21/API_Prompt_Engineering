f5141d563f9756e2c6d798f555c64df2fb345011
NotF-21 <marcellreynaldoo@gmail.com>
2023-12-19 09:47:52 +0700

    Revisi pilihan kota & bahasa 19/12

--- routes.js
+++ routes.js
@@ -6,8 +6,34 @@ const router = express.Router();
 const API_KEY = "9bf25b372702bb91871f585a35997be0";
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ""; // Replace with your actual API key
 
+const cityMap = new Map();
+
+cityMap.set("sby", `https://api.openweathermap.org/data/2.5/weather?lat=-7.2459717&lon=112.7378266&appid=${API_KEY}&units=metric`);
+cityMap.set("bnd", `https://api.openweathermap.org/data/2.5/weather?lat=-7.4419024&lon=112.3956465&appid=${API_KEY}&units=metric`);
+cityMap.set("jkt", `https://api.openweathermap.org/data/2.5/weather?lat=-6.1753942&lon=106.827183&appid=${API_KEY}&units=metric`);
+
+const cityReq = new Map();
+cityReq.set("sby", "Surabaya");
+cityReq.set("bnd", "Bandung");
+cityReq.set("jkt", "Jakarta");
+
+const lanReq = new Map();
+lanReq.set("id", "Bahasa Indonesia");
+lanReq.set("en", "English");
+
 const getCurrentWeather = async () => {
     const link = `https://api.openweathermap.org/data/2.5/weather?lat=-7.2459717&lon=112.7378266&appid=${API_KEY}&units=metric`;
+    const query = await axios.get(link);
+    return {
+        weather: query.data.weather[0].main,
+        temperature: query.data.main.temp,
+        wind_speed: query.data.wind.speed,
+        humidity: query.data.main.humidity,
+    };
+};
+
+const getCityWeather = async (city) => {
+    const link = cityMap.get(city);
     const query = await axios.get(link);
     return {
         weather: query.data.weather[0].main,
@@ -25,9 +51,17 @@ const getImage = async () => {
     };
 };
 
+const getCityImage = async (city) => {
+    const link = cityMap.get(city);
+    const query = await axios.get(link);
+    return {
+        image : query.data.weather[0].icon,
+    };
+};
+
 router.get("/", async (req, res) => {
     try {
-        const weatherData = await getCurrentWeather();
+        const weatherData = await getCityWeather("bnd");
         return res.status(200).send(weatherData);
     } catch (error) {
         console.error('Error:', error.message);
@@ -35,26 +69,43 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.get("/naration", async (req, res) => {
+router.get("/naration/:city/:lan", async (req, res) => {
+    console.log(req.params);
+    let {city, lan} = req.params;
+
     try {
-        const weatherData = await getCurrentWeather();
-        const image = await getImage();
+        const weatherData = await getCityWeather(city);
+        const image = await getCityImage(city);
         const requestData = {
             model: 'gpt-3.5-turbo-0613',
             messages: [
-                { role: 'user', content: 'What is the weather like in Surabaya? Add some naration to better explain.' },
+                { role: 'user', content: 'What is the weather like in the given city? Add some naration to better explain like recomendation and warning.' },
                 {
                     role: 'assistant',
                     content: null,
                     function_call: {
                         name: 'get_current_weather',
-                        arguments: JSON.stringify(weatherData),
+                        arguments: JSON.stringify({
+                            city: cityReq.get(city),
+                            language: lanReq.get(lan),
+                            weather: weatherData.weather,
+                            temperature: weatherData.temperature,
+                            wind_speed: weatherData.wind_speed,
+                            humidity: weatherData.humidity,
+                        }),
                     },
                 },
                 {
                     role: 'function',
                     name: 'get_current_weather',
-                    content: JSON.stringify(weatherData),
+                    content: JSON.stringify({
+                        city: cityReq.get(city),
+                        language: lanReq.get(lan),
+                        weather: weatherData.weather,
+                        temperature: weatherData.temperature,
+                        wind_speed: weatherData.wind_speed,
+                        humidity: weatherData.humidity,
+                    }),
                 },
             ],
             functions: [
@@ -64,9 +115,13 @@ router.get("/naration", async (req, res) => {
                     parameters: {
                         type: 'object',
                         properties: {
-                            location: {
+                            city: {
                                 type: 'string',
-                                description: 'The city and state, e.g. San Francisco, CA',
+                                description: 'The city, e.g., Surabaya, Bandung, Jakarta',
+                            },
+                            language: {
+                                type: 'string',
+                                description: 'The language for the response, either "English" or "Bahasa Indonesia"',
                             },
                             weather: {
                                 type: 'string',
@@ -85,7 +140,7 @@ router.get("/naration", async (req, res) => {
                                 description: 'The humidity, in %',
                             },
                         },
-                        required: ['location'],
+                        required: ['city', 'language'],
                     },
                 },
             ],
